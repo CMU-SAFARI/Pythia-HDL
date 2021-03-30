f146b1069a1fe5679fa547b82791a9748d8915d7
Rahul Bera <rahbera@safari.ethz.ch>
2021-03-31 01:27:58 +0200

    Added support: 2 read ports in Plane

--- src/main/scala/pythia/Plane.scala
+++ src/main/scala/pythia/Plane.scala
@@ -1,5 +1,10 @@
-// Author: Rahul Bera
-// Description: Implements a 128x16 plane of QVStore
+/**********************************************************
+ * Author: Rahul Bera
+ * Description: Implements a 128x16 plane of QVStore
+ * Read ports: 2
+ * Write ports: 1
+ * ********************************************************/
+
 package pythia
 
 import chisel3._
@@ -12,24 +17,31 @@ class Plane extends Module {
     val wrcol = Input(UInt(4.W))
     val wrdata = Input(UInt(16.W))
     val re = Input(Bool())
-    val rdrow = Input(UInt(7.W))
-    val rdcol = Input(UInt(4.W))
-    val rddata = Output(UInt(16.W))
+    val rdrow0 = Input(UInt(7.W))
+    val rdcol0 = Input(UInt(4.W))
+    val rddata0 = Output(UInt(16.W))
+    val rdrow1 = Input(UInt(7.W))
+    val rdcol1 = Input(UInt(4.W))
+    val rddata1 = Output(UInt(16.W))
   })
 
   val mem = Mem(2048, UInt(16.W))
 
-  val rdindex = Wire(UInt(11.W))
+  val rdindex0 = Wire(UInt(11.W))
+  val rdindex1 = Wire(UInt(11.W))
   val wrindex = Wire(UInt(11.W))
-  rdindex := (io.rdrow << 4) + io.rdcol
+  rdindex0 := (io.rdrow0 << 4) + io.rdcol0
+  rdindex1 := (io.rdrow1 << 4) + io.rdcol1
   wrindex := (io.wrrow << 4) + io.wrcol
 
   when(io.we) {
     mem(wrindex) := io.wrdata
   }
 
-  io.rddata := 0.U
+  io.rddata0 := 0.U
+  io.rddata1 := 0.U
   when(io.re) {
-    io.rddata := mem(rdindex)
+    io.rddata0 := mem(rdindex0)
+    io.rddata1 := mem(rdindex1)
   }
 }
--- src/test/scala/pythia/PlaneTests.scala
+++ src/test/scala/pythia/PlaneTests.scala
@@ -1,16 +1,18 @@
-// See LICENSE.txt for license details.
 package pythia
 
 import chisel3.iotesters.{PeekPokeTester, Driver, ChiselFlatSpec}
 
 
 class PlaneTests(c: Plane) extends PeekPokeTester(c) {
-  def PlaneRead(row:Int, col:Int, data:Int){
-    poke(c.io.rdrow, row)
-    poke(c.io.rdcol, col)
+  def PlaneRead(row0:Int, col0:Int, data0:Int, row1:Int, col1:Int, data1:Int){
+    poke(c.io.rdrow0, row0)
+    poke(c.io.rdcol0, col0)
+    poke(c.io.rdrow1, row1)
+    poke(c.io.rdcol1, col1)
     poke(c.io.re, 1)
     step(1)
-    expect(c.io.rddata, data)
+    expect(c.io.rddata0, data0)
+    expect(c.io.rddata1, data1)
   }
   def PlaneWrite(row:Int, col:Int, data:Int){
     poke(c.io.wrrow, row)
@@ -19,30 +21,16 @@ class PlaneTests(c: Plane) extends PeekPokeTester(c) {
     poke(c.io.we, 1)
     step(1)
   }
-  /*
-  def PlaneRead(addr:Int, data:Int) = {
-    poke(c.io.re, 1)
-    poke(c.io.rdaddr, addr)
-    step(1)
-    expect(c.io.rddata, data)
-  }
-  def PlaneWrite(addr:Int, data:Int) = {
-    poke(c.io.we, 1)
-    poke(c.io.wraddr, addr)
-    poke(c.io.wrdata, data)
-    step(1)
-  }
-  */
   for(t <- 0 until 9){
-    val row = rnd.nextInt(128)
-    val col = rnd.nextInt(16)
-    val data = rnd.nextInt(65536)
-    PlaneWrite(row, col, data)
-    PlaneRead(row, col, data)
-    //val addr = rnd.nextInt(2048)
-    //val data = rnd.nextInt(65536)
-    //PlaneWrite(addr, data)
-    //PlaneRead(addr, data)
+    val row0 = rnd.nextInt(128)
+    val col0 = rnd.nextInt(16)
+    val data0 = rnd.nextInt(65536)
+    val row1 = rnd.nextInt(128)
+    val col1 = rnd.nextInt(16)
+    val data1 = rnd.nextInt(65536)
+    PlaneWrite(row0, col0, data0)
+    PlaneWrite(row1, col1, data1)
+    PlaneRead(row0, col0, data0, row1, col1, data1)
   }
 }
 
